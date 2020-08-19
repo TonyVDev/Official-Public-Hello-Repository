63272eaf3ee8e39f70faa05e9fe6377a20657a08
TonyVDev <69826730+TonyVDev@users.noreply.github.com>
2020-08-19 13:29:22 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Anthony Vinton
+ *         avinton6@huskers.unl.edu
+ * Date: 08/17/2020
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Anthony Vinton\n");
+
+  return 0;
+}
